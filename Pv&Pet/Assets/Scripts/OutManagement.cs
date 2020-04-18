954aebc93261735ca68726bbd43795b2edd9e215
jrafael11 <a16110302@ceti.mx>
2020-04-18 18:47:38 -0500

    Revert "Merge branch 'PvP_Development' into Pve_Development"
    This reverts commit 65bddb9395eb00420a7532494f97755efdea49c8, reversing
    changes made to 18b675e5ced9b2813a6514f784633e9e7508b9c2.

---
+++ Pv&Pet/Assets/Scripts/OutManagement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class OutManagement : MonoBehaviour
+{
+    public static Firebase.Auth.FirebaseAuth auth;
+    public static Firebase.Auth.FirebaseUser user;
+    private string displayName;
+    private bool signedIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeFirebase();
+    }
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                displayName = user.DisplayName ?? "";
+                Debug.Log("Signed in " + user.UserId);
+
+            }
+        }
+    }
+    public void SignOut()
+    {
+        auth.SignOut();
+        SceneManager.LoadScene("00-Login");
+
+    }
+}
---
+++ Pv&Pet/Assets/Scripts/PVP.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.EventSystems;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine.SceneManagement;
+
+public class PVP : MonoBehaviour
+{
+    DatabaseReference reference;
+    public GameObject FigtherButton;
+
+    void Start()
+    {
+        DB();
+        GetUserStats();
+    }
+
+
+    public void Refreshlist()
+    {
+        DB();
+        GetUserStats();
+        //Getdata();
+    }
+
+    void DB()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://pvpet-f0b05.firebaseio.com/Players/Pa5UU16uCzt6X1E1DJ6a");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+    void GetUserStats()
+    {
+        string uid;
+        Text textuserid = GameObject.Find("Canvas/Txt_userid").GetComponent<Text>();
+        uid = "4cx9WViw9RTIKjNyW9deBgkkn7y1";
+
+        Text textUsername = GameObject.Find("Canvas/lbl_username").GetComponent<Text>();
+        Text textHP = GameObject.Find("Canvas/lbl_pv").GetComponent<Text>();
+        Text textAgility = GameObject.Find("Canvas/lbl_ag").GetComponent<Text>();
+        Text textSpeed = GameObject.Find("Canvas/lbl_sp").GetComponent<Text>();
+        Text textStrength = GameObject.Find("Canvas/lbl_str").GetComponent<Text>();
+        Text textArmorv = GameObject.Find("Canvas/lbl_arm").GetComponent<Text>();
+
+
+
+        FirebaseDatabase.DefaultInstance.GetReference("users").Child(uid).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("DataManager: read database is faulted with error: " + task.Exception.ToString());
+                return;
+            }
+
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                Debug.Log("DataManager: Read scores");
+
+                Dictionary<string, System.Object> attributes = (Dictionary<string, System.Object>)snapshot.Value;
+
+                if (snapshot.Exists)
+                {
+                    Debug.Log("DataManager: UID ==> " + attributes["username"].ToString());
+                    Debug.Log("DataManager: HP ==> " + attributes["HP"].ToString());
+                    Debug.Log("DataManager: Agility ==> " + attributes["Agility"].ToString());
+                    Debug.Log("DataManager: Speed ==> " + attributes["Speed"].ToString());
+                    Debug.Log("DataManager: Strength ==> " + attributes["Strength"].ToString());
+                    Debug.Log("DataManager: Armorv ==> " + attributes["Armorv"].ToString());
+
+                    textUsername.text = attributes["username"].ToString();
+                    textHP.text = "HP:" + attributes["HP"].ToString();
+                    textAgility.text = "AGY:" + attributes["Agility"].ToString();
+                    textSpeed.text = "SPE:" + attributes["Speed"].ToString();
+                    textStrength.text = "STR:" + attributes["Strength"].ToString();
+                    textArmorv.text = "ARM:" + attributes["Armorv"].ToString();
+                }
+                else
+                {
+                    Debug.LogError("DataManager: Database for the user not available.");
+                }
+
+            }
+        });
+    }
+    void Getdata()
+    {
+        Firebase.Database.FirebaseDatabase dbInstance = Firebase.Database.FirebaseDatabase.DefaultInstance;
+        dbInstance.GetReference("users").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                foreach (DataSnapshot user in snapshot.Children)
+                {
+                    IDictionary dictUser = (IDictionary)user.Value;
+                    Debug.Log(dictUser["username"].ToString());
+                }
+            }
+        });
+        MyAwesomeCreator();
+    }
+    void MyAwesomeCreator()
+    {
+        GameObject tButton = (GameObject)Instantiate(FigtherButton);
+        Button buttonCtrl = tButton.GetComponent<Button>();
+        buttonCtrl.onClick.AddListener(() => FooOnClick());
+    }
+    void FooOnClick()
+    {
+        Debug.Log("as");
+    }
+}
