1e18839c81577486a10a246425375bf5c65cac0e
jrafael11 <a16110302@ceti.mx>
2020-04-21 20:31:26 -0500

    Navegation
    Se cambio el comportamiento de los botones del las distintas escenas para poder acceder a todas.

---
+++ Pv&Pet/Assets/Scripts/ShowPassword.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class ShowPassword : MonoBehaviour
+{
+    public InputField password;
+
+    private void OnMouseDown()
+    {
+        password.contentType = InputField.ContentType.Standard;
+    }
+
+}
