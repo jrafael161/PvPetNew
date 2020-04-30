01126c3739cff1489f24c14c1afdb3d94b314f53
jrafael11 <a16110302@ceti.mx>
2020-04-30 14:10:02 -0500

    Revert "Revert "FlappyBirdRaw""
    This reverts commit cec5608ac95209cd103578d84f03daa66333a97b.

---
+++ Pv&Pet/Sulejotrytopush/Assets/Scenes/Scripts Game1/Game1_jump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game1_jump : MonoBehaviour
+{
+	public float speed = 1;
+	private Rigidbody rb;
+	public LayerMask groundLayers;
+	public float jumpForce = 10;
+	public SphereCollider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+		col = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+        float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal,0,moveVertical);
+		rb.AddForce(movement * speed);
+		
+		if(IsGrounded()&& Input.GetKeyDown(KeyCode.Space) || (IsGrounded()&& Input.touchCount>0)) //alt+124
+		{
+			rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+			rb.velocity=rb.velocity*3.5f;
+		}
+
+    }
+	private bool IsGrounded()
+	{
+		return Physics.CheckCapsule(col.bounds.center,new Vector3(col.bounds.center.x,col.bounds.min.y,col.bounds.center.z),
+		col.radius*.9f,groundLayers);
+	}
+}
---
+++ Pv&Pet/Sulejotrytopush/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+    public Sprite PlayerSprite;
+    public int HP;    
+    public int Level;
+    public int XP;
+    public int Strength;
+    public int Speed;
+    public int Agility;
+    public int Armor;
+    public int PvPCoin;
+    public int PetCoin;
+    public int PremiumCoin;
+    public Item HeadGear;
+    public Item ChestGear;
+    public Item ArmsGear;
+    public Item LegsGear;
+    public Item FootsGear;
+    public Item Weapon;    
+    public List<Item> EquipedItems;
+    public List<Item> Inventory;
+}
