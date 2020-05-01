59045337ba03019f51dc3878344907854f2b561e
Sulejo9 <larasebastian55@hotmail.com>
2020-05-01 17:02:06 -0500

    Flappy Bird
    Submit Flappy Bird game Completed, missing to merge with new sprites and coin system

---
+++ Pv&Pet/Sulejo new Try/FlappyBird/LogicaChar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaChar : MonoBehaviour
+{
+	public float velocity = 1;
+	private Rigidbody2D rb;
+	
+	public ControladorEscena controladorEscena;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+		{
+			rb.velocity=Vector2.up*velocity;
+		}
+    }
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		controladorEscena.Perdiste();
+	}
+}
---
+++ Pv&Pet/Sulejo new Try/FlappyBird/LogicaObstaculos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaObstaculos : MonoBehaviour
+{
+	public float tiempoMax = 1; 
+	private float tiempoInicial = 0;
+	public GameObject obstaculo;
+	public float altura;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obstaculoNuevo = Instantiate(obstaculo);
+		obstaculoNuevo.transform.position = transform.position+ new Vector3(0,0,0);
+		Destroy(obstaculoNuevo,20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(LogicaScore.puntaje <=0 )
+				MovePipe.speed=.2F;
+		else if(LogicaScore.puntaje%2 == 0 & tiempoInicial>tiempoMax)
+		{
+				MovePipe.speed+=.2F;
+		}
+		
+		if(tiempoInicial>tiempoMax)
+		{
+			GameObject obstaculoNuevo = Instantiate(obstaculo);
+			obstaculoNuevo.transform.position = transform.position+ new Vector3(0,Random.Range(-altura,altura),0);
+			Destroy(obstaculoNuevo,20);
+			tiempoInicial=0;
+		}
+		else
+		{
+			tiempoInicial+=Time.deltaTime;
+		}
+		
+		
+    }
+}
