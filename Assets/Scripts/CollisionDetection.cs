a59c2b9f50d6781cf797a19b31ea0ab6f4083605
jrafael11 <a16110302@ceti.mx>
2020-05-11 22:01:55 -0500

    Agility Minigame Implementation

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    static AgilityGame ag;
+
+    private void Start()
+    {
+        ag = GameObject.FindObjectOfType<AgilityGame>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("El jugador choco con algo");
+        }
+        if (collision.collider.tag == "ExplosiveProjectiles")
+        {
+            Debug.Log("PUM");
+            ag.ExplodeProjectile(collision.collider.gameObject);
+            //Mandar a llamar la funcion que desperdigue el meteorito
+        }
+    }
+}
